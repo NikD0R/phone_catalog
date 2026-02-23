bd7aa6d3dbdf6fa718241a9ba454e7918cb92bc4
Nikita Doroshenko <nikitokd05@gmail.com>
2026-02-23 14:08:31 +0200

    phone_catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
+import { Outlet } from 'react-router-dom';
 import './App.scss';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { FavoritesProvider } from './context/Favoutires';
+import { CartProvider } from './context/Cart';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
+export const App = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <CartProvider>
+      <FavoritesProvider>
+        <Header />
+        <main className="main">
+          <Outlet />
+        </main>
+        <Footer />
+      </FavoritesProvider>
+    </CartProvider>
   );
 };
